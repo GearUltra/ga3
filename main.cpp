532cf23a0b49e074e90882ce8c33e2e26b4a5370
Hernan Cabrera <91387687+GearUltra@users.noreply.github.com>
2023-04-08 23:34:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,29 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <vector>
 #include "ArgumentManager.h"
 #include "btree.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-
-    return 0;
+  ArgumentManager am(argc, argv);
+  ifstream fin(am.get("input"));
+  ofstream fout(am.get("output"));
+    
+  string str_num;
+  int num;
+  vector<int> array;
+  
+  while(fin>>str_num){
+    num=stoi(str_num);
+    array.push_back(num);
+  }
+  
+  for(int i =0;i<array.size();i++){
+    tree.insert(array[i]);
+  }
+    
+  return 0;
 }
